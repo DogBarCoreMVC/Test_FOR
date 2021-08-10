c44116ac86eac8d1a1c265e0f5f4089f214cc627
GitDogBar <Dogbar>
2021-08-11 00:48:17 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 1, limit = 3; count <= limit; count ++, limit --)
+            {
+                Console.WriteLine("Count = " + count + " limit = " + limit);
+            }
+        }
+    }
+}
